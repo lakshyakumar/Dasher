e5f14703cef215c6771b3987d607668594e0a56e
Kumar, D. bd <d.bd.kumar@accenture.com>
2020-05-21 20:53:08 +0530

    Adding recursive functionality
    Signed-off-by: Kumar, D. bd <d.bd.kumar@accenture.com>

--- utilities/requests.js
+++ utilities/requests.js
@@ -10,7 +10,39 @@ function get(tag ,url, callBack, configurations={}){
         }
         axios.get(url,config).then(data=>{
             callBack(data.data);
+        }).catch(e =>{
+            console.log(e);
         })
 }
 
+
+function closedIssues(tag, url, callBack, configurations={}){
+    let config = {
+        ...configurations,
+        auth:{
+            username: process.env.GIT_USERNAME,
+            password: process.env.GIT_TOKEN
+        }
+    }
+     axiosRequest(url, 1, callBack, [])
+}
+
+function axiosRequest(url, page, callback, array){
+    let tempURL = `${url}&page=${page}&per_page=100`;
+    axios.get(tempURL).then(
+      data => {
+          console.log(page);
+        if(data.data.length == 100){
+          array = [...array,...data.data]
+          axiosRequest(url,page+1, callback,array)
+        }else{
+          array = [...array,...data.data]
+          callback(array)
+        } 
+      } 
+    )
+  }
+
+
+exports.closedIssues = closedIssues
 exports.get = get;
