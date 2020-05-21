ef775aaaece3154b14943205cb4274248e00a4c8
lakshyakumar <lakshya.kumar@accenture.com>
2020-05-21 20:56:26 +0530

    adding changes to frontend
    Signed-off-by: lakshyakumar <lakshya.kumar@accenture.com>

--- src/utils/data/metrics.js
+++ src/utils/data/metrics.js
@@ -1,4 +1,4 @@
-import { faCodeBranch, faStar, faClone, faBinoculars, faEye, faUser, faFolderOpen, faFolder, faFileImport, faHistory, } from '@fortawesome/free-solid-svg-icons'
+import { faCodeBranch, faStar, faClone, faBinoculars, faAward, faArchive, faEye, faUser, faFolderOpen, faFolder, faFileImport, faHistory, } from '@fortawesome/free-solid-svg-icons'
 
 export default {
     metrics: {
@@ -17,10 +17,22 @@ export default {
         },
         {
           "name": "Contributors",
+          "icon": faAward,
+          "total": 23,
+          "today": 2
+        },
+        {
+          "name": "Unique Visitors",
           "icon": faUser,
           "total": 23,
           "today": 2
-        }
+        },
+        {
+          "name": "Views",
+          "icon": faEye,
+          "total": 23,
+          "today": 2
+        },
       ],
       maintenance:[
         {
@@ -52,6 +64,20 @@ export default {
         {
           "name": "Forked",
           "icon": faCodeBranch,
+          "total": 23,
+          "today": 2
+
+        },
+        {
+          "name": "Clones",
+          "icon": faArchive,
+          "total": 23,
+          "today": 2
+
+        },
+        {
+          "name": "Unique Clones",
+          "icon": faClone,
           "total": 23,
           "today": 2
 
